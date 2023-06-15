cc8800b61552c1d7f8a4bb60d144f210ac62b417
brianmchugh <brianmchugh02@gmail.com>
2023-06-14 20:39:24 -0400

    Getting familiar with the use of the Library class and the add_book and has_book methods

--- Library.py
+++ Library.py
@@ -1,8 +1,20 @@
 from main import Book, Library
 
+#Create book object
 myBook = Book(isbn='1234',title='Python 101',author='Bob Marley')
 print(myBook.isbn,myBook.title,myBook.author)
 
+# Create a Library object
 myLibrary = Library()
 
+# Add the book to the library
+myLibrary.add_book(myBook)
+
+# Check if the book was added
+if myLibrary.has_book("1234"):
+    print("The book was added to the library.")
+else:
+    print("The book was not found in the library.")
+
+# Output: The book was added to the library.
 
