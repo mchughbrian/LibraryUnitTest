53cd69c9266977ef4189cc793b0450147e838e90
brianmchugh <brianmchugh02@gmail.com>
2023-06-14 20:47:33 -0400

    Getting familiar with the use of the Library class and the remove_book method and error handling

--- Library.py
+++ Library.py
@@ -18,4 +18,20 @@ else:
 
 # Output: The book was added to the library.
 
+#use the count books, output should be 1
 print(myLibrary.count_books())
+
+#remove the book from myLibrary
+myLibrary.remove_book('1234')
+
+#check to make sure it was removed
+print(myLibrary.count_books())
+
+# Add the book to the library
+myLibrary.add_book(myBook)
+
+#try to remove a book that does not exist and handle the error appropriately
+try:
+    myLibrary.remove_book('4567')
+except ValueError as e:
+    print(e)
